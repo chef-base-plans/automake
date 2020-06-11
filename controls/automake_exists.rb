a469c396d80acff1d2dfae6d6cd49e6b93433ddc
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-11 14:55:41 +0100

    Remove DEBUG=true; replace hab pkg exec with full path to binary; and parse the pkg_version from installation path
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/automake_exists.rb
+++ controls/automake_exists.rb
@@ -2,12 +2,12 @@ title 'Tests to confirm automake exists'
 
 plan_origin = ENV['HAB_ORIGIN']
 plan_name = input('plan_name', value: 'automake')
- 
+
 control 'core-plans-automake-exists' do
   impact 1.0
   title 'Ensure automake exists'
   desc '
-  Verify automake by ensuring /bin/automake exists'
+  Verify automake by ensuring bin/automake exists'
   
   plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
   describe plan_installation_directory do
@@ -15,8 +15,8 @@ control 'core-plans-automake-exists' do
     its('stdout') { should_not be_empty }
   end
 
-  command_relative_path = input('command_relative_path', value: '/bin/automake')
-  command_full_path = plan_installation_directory.stdout.strip + "#{command_relative_path}"
+  command_relative_path = input('command_relative_path', value: 'bin/automake')
+  command_full_path = File.join(plan_installation_directory.stdout.strip, "#{command_relative_path}")
   describe file(command_full_path) do
     it { should exist }
   end
