f2167faa568d143d1945a627c0b4d67bab685444
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-26 11:30:12 +0100

    Update documentation
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/automake_works.rb
+++ controls/automake_works.rb
@@ -7,9 +7,9 @@ control 'core-plans-automake-works' do
   impact 1.0
   title 'Ensure automake works as expected'
   desc '
-  Verify automake by ensuring 
-  (1) its installation directory exists and 
-  (2) that it returns the expected version.
+  Verify automake by ensuring that
+  (1) its installation directory exists 
+  (2) binaries return the expected version
   '
   
   plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
@@ -19,13 +19,20 @@ control 'core-plans-automake-works' do
     its('stderr') { should be_empty }
   end
   
-  command_relative_path = input('command_relative_path', value: 'bin/automake')
-  command_full_path = File.join(plan_installation_directory.stdout.strip, "#{command_relative_path}")
+  automake_full_path = File.join(plan_installation_directory.stdout.strip, "bin/automake")
   plan_pkg_version = plan_installation_directory.stdout.split("/")[5]
-  describe command("#{command_full_path} --version") do
+  describe command("#{automake_full_path} --version") do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
     its('stdout') { should match /automake \(GNU automake\) #{plan_pkg_version}/ }
+    its('stderr') { should be_empty }
+  end
+
+  aclocal_full_path = File.join(plan_installation_directory.stdout.strip, "bin/aclocal")
+  describe command("#{aclocal_full_path} --version") do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should_not be_empty }
+    its('stdout') { should match /aclocal \(GNU automake\) #{plan_pkg_version}/ }
     its('stderr') { should be_empty }
   end
 end
